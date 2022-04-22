764503f978baab7a3c63f4cff31b10e8bd1aae77
DarkGypsy <102004273+DarkGypsy@users.noreply.github.com>
2022-04-21 23:25:36 -0400

    Add files via upload

---
+++ tarea.js
@@ -0,0 +1,7 @@
+alert("Otros devs te quieren conocer...")
+var num1 = prompt("Ingrese un numero");
+var num2 = prompt( "Ingrese un segundo numero")
+ var x = parseInt(num1);
+ var y = parseInt(num2);
+var respuesta = x + y;
+document.write("El resultado es " + respuesta)
